f9db2c93b6269ebe56f2bef99b922639a5b18b17
Sam Greenwood <sam@samgreenwood.me>
2015-01-21 23:59:09 +1030

    - tagger and test

---
+++ app/tests/TagTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Mockery as m;
+use Swapshop\Tag\Tagable;
+use Illuminate\Support\Str;
+use Swapshop\Tag\TagRepositoryInterface;
+
+class TagTest extends TestCase
+{
+    public function test_tagger_creates_non_existant_tags()
+    {
+        $tagRepositoryInterface = m::mock(TagRepositoryInterface::class)->shouldReceive('save')->once()->shouldReceive('getBySlug')->once()->with('test-name')->getMock();
+        $str = m::mock(Str::class)->shouldReceive('slug')->once()->with('Test Name')->andReturn('test-name')->getMock();
+        $tagable = m::mock(Tagable::class)->shouldReceive('setTags')->once()->getMock();
+
+        $tagger = new \Swapshop\Tag\Tagger($tagRepositoryInterface, $str);
+        $tagger->tag($tagable, ['Test Name']);
+    }
+}
--- src/Swapshop/Tag/Tagger.php
+++ src/Swapshop/Tag/Tagger.php
@@ -1,6 +1,7 @@
 <?php namespace Swapshop\Tag;
 
-use Swapshop\Tag\Tag\TagRepositoryInterface;
+use Illuminate\Support\Str;
+use Swapshop\Tag\TagRepositoryInterface;
 
 class Tagger
 {
@@ -8,29 +9,44 @@ class Tagger
      * @var TagRepositoryInterface
      */
     private $tagRepository;
+    /**
+     * @var Str
+     */
+    private $str;
 
     /**
      * @param TagRepositoryInterface $tagRepository
+     * @param Str $str
      */
-    public function __construct(TagRepositoryInterface $tagRepository)
+    public function __construct(TagRepositoryInterface $tagRepository, Str $str)
     {
         $this->tagRepository = $tagRepository;
+        $this->str = $str;
     }
 
     /**
-     * Set the tags on a tagable entity
-     *
      * @param Tagable $tagable
-     * @param array $tags
+     * @param array $tagNames
      */
-    public function tag(Tagable $tagable, $tags = [])
+    public function tag(Tagable $tagable, array $tagNames = [])
     {
-        $tags = array_filter($tags, function($tag)
-        {
-            return $tag instanceof Tag;
-        });
+        $tags = [];
+
+        foreach ($tagNames as $tagName) {
+
+            $slug = $this->str->slug($tagName);
+
+            if ( ! $t = $this->tagRepository->getBySlug($slug)) {
+                $tag = new Tag;
+                $tag->name = $tagName;
+                $this->tagRepository->save($tag);
+            }
+
+            $tagArr[] = $tag;
+        }
 
         $tagable->setTags($tags);
 
     }
+
 }
