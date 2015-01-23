28ba6db39a009d2efd823430abb615bf49a95182
Sam Greenwood <sam.greenwood@codium.com.au>
2015-01-23 16:08:03 +1030

    - login form updates

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -1,95 +1,54 @@
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Air-Stream Swapshop</title>
-  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <title>Swapshop</title>
 
-  <!-- For sample logo only-->
-  <!--Remove if you no longer need this font-->
-  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Aguafina+Script">
-  <!--For sample logo only-->
+    <!-- Bootstrap core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.1/yeti/bootstrap.min.css" rel="stylesheet" />
 
+    <!-- Custom styles for this template -->
+    <link href="/stylesheets/signin.css" rel="stylesheet">
 
-  <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">
-  <link rel="stylesheet" href="lib/font-awesome/css/font-awesome.css">
-  <script src="lib/jquery-1.7.2.min.js" type="text/javascript"></script>
-  <script src="javascripts/site.js" type="text/javascript"></script>
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
-  <link rel="stylesheet" type="text/css" href="stylesheets/theme.css">
-
-
-  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-      <![endif]-->
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-      <!-- Le fav and touch icons -->
-      <link rel="shortcut icon" href="../assets/ico/favicon.ico">
-      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-    </head>
+  <body>
 
-    <!--[if lt IE 7 ]> <body class="ie ie6"> <![endif]-->
-    <!--[if IE 7 ]> <body class="ie ie7 "> <![endif]-->
-    <!--[if IE 8 ]> <body class="ie ie8 "> <![endif]-->
-    <!--[if IE 9 ]> <body class="ie ie9 "> <![endif]-->
-    <!--[if (gt IE 9)|!(IE)]><!--> 
-    <body class=""> 
-      <!--<![endif]-->
-
-
-      <div class="row-fluid login">
-        <div class="dialog">
-          <p class="brand" href="/">Air-Stream<br />Swapshop.</p>
-          <div class="block">
-            <div class="block-header">
-              <h2>Sign In</h2>
-            </div>
-            <form method="POST">
-              <label>Username</label>
-              <input type="text" class="span12" name="username">
-              <label>Password</label>
-              <input type="password" class="span12" name="password">
-              <div class="form-actions">
-                <input type='submit' class="btn btn-success pull-right" value='Sign In'></input>
-                <div class="clearfix"></div>
-              </div>
-            </form>
-          </div>
-          @if(Session::has('error'))
-          <div class="alert alert-error fade in">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            {{Session::get('error')}}
-          </div>
-          @endif
-          @if(Session::has('message'))
-          <div class="alert alert-information fade in">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            {{Session::get('message')}}
-          </div>
-          @endif
+    <div class="container">
+      <form class="form-signin" method="POST" action="/login">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="username" class="sr-only">Username</label>
+        <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
+        <label for="password" class="sr-only">Password</label>
+        <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
         </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
 
-      </div>
-
-
-
-      <script src="lib/bootstrap/js/bootstrap.js"></script>
-      <script type="text/javascript">
-        $("[rel=tooltip]").tooltip();
-        $(function() {
-          $('.demo-cancel-click').click(function(){return false;});
-        });
-      </script>
-
-    </body>
-    </html>
+    </div> <!-- /container -->
 
 
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
