7952b5617e3f8fcd745de1e5d6dc0e651a8fec95
Sam Greenwood <sam.greenwood@codium.com.au>
2015-01-23 16:00:39 +1030

    - add select2 for dropdowns and tag picker

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,22 +1,39 @@
-$(function(){
-    $('#phone-navigation #phone-menu').change(function(){
+$(function () {
+    $('#phone-navigation #phone-menu').change(function () {
         var url = $(this).val();
-        if(url) {
+        if (url) {
             window.location = url;
         }
     });
+
+    $('select').select2();
+
+    if ($('.tags').length) {
+        $.getJSON('/api/tags', function (tags) {
+            var selectTags = [];
+            $.each(tags, function (id, tag) {
+                selectTags.push(tag)
+            });
+            $('.tags').select2({
+                multiple: true,
+                width: '100%',
+                tags: selectTags
+            });
+        })
+    }
+
 });
 
-var fancyFilter = function(filterListSelector, gallerySelector) {
+var fancyFilter = function (filterListSelector, gallerySelector) {
     //Filter Button Code
-    $(filterListSelector + ' a').click(function() {
+    $(filterListSelector + ' a').click(function () {
         $(filterListSelector + ' li').removeClass('active');
         var $this = $(this);
         var filterType = $this.data('filter');
-        if(!filterType) return true;
+        if (!filterType) return true;
 
         $this.closest('li').addClass('active');
-        $(gallerySelector).isotope({ 
+        $(gallerySelector).isotope({
             filter: filterType,
         });
 
